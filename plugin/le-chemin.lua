9948a88cc1b2845ba6073b8dc8f2547270ae1c3b
jean-charles.verdier <jean-charles.verdier@flare.io>
2026-02-03 21:16:48 -0500

    changed absolute path shortkey

--- plugin/le-chemin.lua
+++ plugin/le-chemin.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
     keys = {
       { "<leader>cp", "<cmd>LeCheminRelative<cr>", desc = "Copy Relative Path" },
-      { "<leader>ca", "<cmd>LeCheminAbsolute<cr>", desc = "Copy Absolute Path" },
+      { "<leader>cP", "<cmd>LeCheminAbsolute<cr>", desc = "Copy Absolute Path" },
     },
   },
 }
